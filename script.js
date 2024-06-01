a92d97aee7d1d459f97fd079a1ed76c509e54aec
syntakxs <109576812+syntakxs@users.noreply.github.com>
2024-06-01 20:09:37 +0800

    Add files via upload

--- script.js
+++ script.js
@@ -1,49 +1,37 @@
-function ryanwagkangsusuko() {
+document.getElementById('toggle-genres').addEventListener('click', function() {
+    var genreContainer = document.getElementById('genre-container');
+    genreContainer.classList.toggle('visible');
+});
 
-  var an = parseFloat(document.getElementById("an").value);
-  var a1 = parseFloat(document.getElementById("a1").value);
-  var n = parseFloat(document.getElementById("n").value);
-  var d = parseFloat(document.getElementById("d").value);
-
-
-  //if the given has first term and the last term
-  if (a1 && an && n ) {
-    var arithmetic = (n/2)*(a1 + an);
-    alert(arithmetic);
-  }
-  else if (a1 && n && d ) {
-    var arithmetic6 = (n/2)*((2*a1)+((n-1)*d));
-    alert(arithmetic6);
-  //if all the input is blank
-  }
-  else if (isNaN(an,a1,n,d)) {
-   alert("Please fill up the blanks");
-  }
- //
-  else if (isNaN(n,d)) {
-    var OptionChosen = prompt("Please enter 1, if finding the sum of odd numbers and 2, if even: ");
-    if (OptionChosen === null ) {
-      return; //break out of the function early
-    }
-    //If we are trying to find the sum of odd numbers
-    else if (OptionChosen == 1) {
-    var arithmetic2 = (a1+an)/2;
-    var arithmetic3 = arithmetic2**2;
-    alert(arithmetic3)
-    }
-    //If we are trying to find the sum of even numbers
-    else if (OptionChosen == 2) {
-    var arithmetic4 = ((a1+an)/2)-1;
-    var arithmetic5 = arithmetic4*(arithmetic4+1);
-    alert(arithmetic5);
-    }
-    else {
-      alert("Please input only the choices 1 and 2.");
-    }
-  }
-  else{
-   alert(":>");
-  }
+function showMore(sectionId) {
+    var section = document.getElementById(sectionId);
+    section.classList.remove('hidden');
+    event.target.style.display = 'none';
 }
 
-//<!-- CREATED BY RYAN :> -->
+function includeHTML() {
+    var elements, i, element, file, xhttp;
+    elements = document.getElementsByTagName("*");
+    for (i = 0; i < elements.length; i++) {
+        element = elements[i];
+        file = element.getAttribute("include-html");
+        if (file) {
+            xhttp = new XMLHttpRequest();
+            xhttp.onreadystatechange = function() {
+                if (this.readyState == 4) {
+                    if (this.status == 200) {element.innerHTML = this.responseText;}
+                    if (this.status == 404) {element.innerHTML = "Page not found.";}
+                    element.removeAttribute("include-html");
+                    includeHTML();
+                }
+            }
+            xhttp.open("GET", file, true);
+            xhttp.send();
+            return;
+        }
+    }
+}
+
+// Call includeHTML function to include HTML files
+includeHTML();
+
